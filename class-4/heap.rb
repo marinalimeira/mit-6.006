e28fc7b0db351f512456a27fb16b845556422f87
Marina Limeira <marinaflessa@gmail.com>
2016-12-26 11:04:49 -0300

    Add Heap methods

--- class-4/heap.rb
+++ class-4/heap.rb
@@ -1,6 +1,13 @@
 class Heap
+  # https://courses.csail.mit.edu/6.006/fall10/handouts/recitation10-8.pdf
+  attr_accessor :arr
+
   def initialize arr
     @arr = arr
+  end
+
+  def size
+    @arr.length
   end
 
   def root
@@ -8,14 +15,45 @@ class Heap
   end
 
   def parent i
-    @arr[(i - 1) / 2]
+    (i - 1) / 2
   end
 
   def left i
-    @arr[2 * i + 1]
+    2 * i + 1
   end
 
   def right i
-    @arr[2 * i + 2]
+    2 * i + 2
+  end
+
+  def build_max_heap
+    (size / 2).downto(0).each { |i| max_heapify i }
+  end
+
+  def max_heapify i
+    l = left i
+    r = right i
+
+    largest = l if l <= size && @arr[l] && @arr[l] > @arr[i] 
+    largest ||= i
+    largest = r if r <= size && @arr[r] && @arr[r] > @arr[largest] 
+
+    if largest != i
+      @arr[i], @arr[largest] = @arr[largest], @arr[i]
+      max_heapify largest 
+    end
+
+    @arr
+  end
+
+  def sort
+    build_max_heap
+    size.downto(1).each do |i|
+      @arr[0], @arr[i] = @arr[i], @arr[0]
+      # @arr.pop
+      max_heapify i
+    end
+
+    @arr
   end
 end
--- spec/heap_spec.rb
+++ spec/heap_spec.rb
@@ -1,28 +1,39 @@
 require './class-4/heap.rb'
 
 describe 'Heap' do
-  #      70
-  #   31    42
-  # 63  14 25
+  #      7
+  #   3    4
+  # 6  1  2
   let(:heap) { Heap.new [7, 3, 4, 6, 1, 2] }
 
   context "#root" do
     it { expect(heap.root).to eq 7 }
   end
 
-  # receives index and returns element
+  # receives index and returns another index
   context "#parent" do
-    it { expect(heap.parent(4)).to eq 3 }
-    it { expect(heap.parent(5)).to eq 4 }
+    it { expect(heap.parent(4)).to eq 1 }
+    it { expect(heap.parent(5)).to eq 2 }
   end
 
   context "#left" do
-    it { expect(heap.right(0)).to eq 4 }
-    it { expect(heap.right(1)).to eq 1 }
+    it { expect(heap.right(0)).to eq 2 }
+    it { expect(heap.right(1)).to eq 4 }
   end
 
   context "#right" do
-    it { expect(heap.left(1)).to eq 6 }
-    it { expect(heap.left(2)).to eq 2 }
+    it { expect(heap.left(1)).to eq 3 }
+    it { expect(heap.left(2)).to eq 5 }
+  end
+
+  context "#max_heapify" do
+    it do
+      expect(heap.max_heapify(1)).to eq [7, 6, 4, 3, 1, 2]
+      expect(heap.arr).to eq [7, 6, 4, 3, 1, 2]
+    end
+  end
+
+  context "#sort" do
+    xit { expect(heap.sort).to eq [1, 2, 3, 4, 6, 7] }
   end
 end
